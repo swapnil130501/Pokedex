8f87ce09b39273c3314ed279d3523e617774ade6
swapnil <swapnil130501@gmail.com>
2024-10-19 21:58:23 +0530

    utility file with typeColor obj

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-
+import { typeColors } from '../utils/utility';
 function PokeCard({ name }) {
     const [data, setData] = useState({});
 
@@ -16,17 +16,21 @@ function PokeCard({ name }) {
     useEffect(() => {
         getPokeInfo(name);
     }, [name]);
+
+    const firstType = data?.types?.[0]?.type?.name;
+    const bgColor = typeColors[firstType]?.bg || "from-gray-700 to-gray-900"; 
+    const hoverColor = typeColors[firstType]?.hover || "hover:from-gray-600 hover:to-gray-800";
     
     return (
-        <div className="bg-white border rounded-lg p-4 shadow-md text-center">
+        <div className={`rounded-lg shadow-lg p-8 max-w-sm mx-auto bg-gradient-to-br ${bgColor} ${hoverColor} hover:shadow-2xl transition-shadow duration-300 ease-in-out cursor-pointer`}>
             {data.sprites?.front_default && (
                 <img
-                    src={data.sprites.front_default}
+                    src={data.sprites.other['official-artwork'].front_default}
                     alt={name}
-                    className="w-24 h-24 mx-auto"
+                    className="w-48 h-48 mx-auto mb-6"
                 />
             )}
-            <h3 className="text-lg font-semibold mt-2">{name}</h3>
+            <h1 className="text-3xl font-bold text-center mb-4 text-white capitalize">{name}</h1>
         </div>
     );
 }
